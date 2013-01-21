93037cf9384b783d19a0938265622d262097ba7d
Andreea <gavrilovici_deea@yahoo.com>
2013-01-21 18:22:45 -0500

    Another commit

--- app/models/name.rb
+++ app/models/name.rb
@@ -2,5 +2,5 @@ class Name < ActiveRecord::Base
   attr_accessible :title
  
   validates :title, uniqueness: true, presence: true, :length => {:maximum => 30}
-  validates :title, :format => { :with => /^((?!http|https):\/\/)?[a-z0-9]+([-.]{1}[a-z0-9]+).[a-z]{2,5}(:[0-9]{1,5})?(\/.)?$/ }
+  validates :title, :format => { :with => /^(?!http|https|www)([a-z0-9]+)$/ }
 end
