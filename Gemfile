943506e3912828436c088d49517f2a8bc21d86ce
Andreea <gavrilovici_deea@yahoo.com>
2013-02-25 18:03:14 -0500

    Create user model by migration

--- Gemfile
+++ Gemfile
@@ -26,7 +26,7 @@ end
 
 gem 'jquery-rails'
 
-gem 'bcrypt-ruby', :require => 'bcrypt' 
+gem 'bcrypt-ruby', :require => 'bcrypt'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130220212232) do
+ActiveRecord::Schema.define(:version => 20130225225003) do
 
   create_table "names", :force => true do |t|
     t.string   "title"
